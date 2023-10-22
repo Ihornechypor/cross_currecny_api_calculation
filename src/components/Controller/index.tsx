9c29616a4e68d6f03d000f698854bc1372c724a8
ihor.nechypor <ihor.nechypor@plej.pl>
2023-10-22 17:01:41 +0200

    updas

--- src/components/Controller/index.tsx
+++ src/components/Controller/index.tsx
@@ -1,6 +1,7 @@
 import { ReactNode, useState, useEffect } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import { compareAsc, format } from 'date-fns';
 
 interface ControllerProps {
   children?: ReactNode;
@@ -8,13 +9,15 @@ interface ControllerProps {
 const Controller = ({ children }: ControllerProps) => {
   const [rate, setRate] = useState(0);
   const [startDate, setStartDate] = useState(null);
-  const [api, setApi] = useState(`http://api.nbp.pl/api/exchangerates/rates/a/usd/${startDate}/?format=json`);
+  const [ammount, setAmmount] = useState(null);
+  const [api, setApi] = useState(null);
+
+  const [rates, setRates] = useState([]);
 
   useEffect(() => {
     fetch(api)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
         setRate(data.rates[0].mid);
       })
       .catch((err) => {
@@ -22,19 +25,39 @@ const Controller = ({ children }: ControllerProps) => {
       });
   }, [startDate]);
 
+  const handleAmmount = (e) => {
+    setAmmount(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    const fee = ammount * rate * 0.01;
+    const amFree = ammount * rate - fee;
+    setRates((prev) => [...prev, { fee: fee, amFree: amFree }]);
+  };
+
   return (
-    <div>
+    <>
       <p>{rate}</p>
-      <p>{startDate}</p>
+      <input type="number" placeholder="ammount" onChange={handleAmmount} /> <br />
       <DatePicker
         selected={startDate}
         onChange={(date) => {
-          console.log(date);
-
-          // setStartDate(date);
+          setStartDate(date);
+          setApi(`http://api.nbp.pl/api/exchangerates/rates/a/usd/${format(date, 'yyyy-MM-dd')}/?format=json`);
         }}
-      />
-    </div>
+      />{' '}
+      <br />
+      <button onClick={handleSubmit}>Claculate price</button>
+      <ul>
+        {rates.map((item, index) => {
+          return (
+            <li key={index}>
+              fee: {item.fee}; clearAmmount: {item.amFree}
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 };
 export default Controller;
